1a0ec490fb80e884066862f8fbb74688366543d0
Ellie Day <heyellieday@gmail.com>
2015-10-09 17:10:12 -0500

    Added puma.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
   def index
 
   	@posts = Post.paginate(page: params[:page], per_page: 5).order('created_at DESC')
